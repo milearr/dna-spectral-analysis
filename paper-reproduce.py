10d69f7bd1940dde21c14c5c23ee5a48339b6241
Milena Arruda <82825020+Milena-Arruda@users.noreply.github.com>
2021-08-19 18:51:32 -0300

    Create paper-reproduce.py

---
+++ paper-reproduce.py
@@ -0,0 +1,17 @@
+import os
+
+# Figure 3
+os.system("python3 main.py -a file-mode -p './database-seq/YPL230W.fasta'")
+
+# Figure 4
+os.system("python3 main.py -a file-mode -p './database-seq/YOR215C.fasta'")
+
+# Figure 5
+os.system("python3 main.py -a file-mode -p './database-seq/YNL122C.fasta'")
+
+# Statistics: Table I, Figure 8 and Figure 9
+os.system("python3 main.py -a database-mode -n 200 './database-cerevisiae/'")
+os.system("python3 main.py -a statistics-mode './database-cerevisiae/'")
+
+# Figure 10
+os.system("python3 main.py -a file-mode -p -sw -wl 351 -st 5 './database-seq/F56F11_4a.fasta'")
