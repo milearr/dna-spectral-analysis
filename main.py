cec252b2477e9343f54eac3bb4664f08606e74ab
Milena Arruda <82825020+Milena-Arruda@users.noreply.github.com>
2021-08-19 11:09:37 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Ago 12 2021
+
+@author: milena-arruda
+"""
+
+from project import *
+
+
+def main():
+    
+    args = initialize()
+
+    if hasattr(args, 'file'):
+        args.dir = os.path.dirname(args.file) + '/'
+        seq = read_fasta(args.file)
+        _ = seq_analysis(args, seq)
+        
+    elif hasattr(args, 'dir'):
+        data_cds, data_intergenic = read_dir_database(args.dir, args.min_length_seq)
+        print(len(data_cds),len(data_intergenic))
+        _ = database_analysis(args, data_cds)
+        _ = database_analysis(args, data_intergenic)
+        _ = write_summarized_results_database(args.dir)
+    
+    elif hasattr(args, 'dir_statistics'):
+        statistics(args)
+
+
+
+
+if __name__ == "__main__":
+    main()
