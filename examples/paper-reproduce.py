2b90948d791934f2aa7d6f5087aa38816ca999c2
Milena-Arruda <milena.arruda@ee.ufcg.edu.br>
2022-07-06 09:55:02 -0300

    add jupyter notebook statistics

--- examples/paper-reproduce.py
+++ examples/paper-reproduce.py
@@ -1,17 +1,13 @@
 import os
 
-# Figure 3
-# os.system("python3 main.py -v file-mode -p './database-seq/YPL230W.fasta'")
-
-# Figure 4
+# Figure 6
 # os.system("python3 main.py -a file-mode -p './database-seq/YOR215C.fasta'")
 
-# Figure 5
+# Figure 7
 # os.system("python3 main.py -a file-mode -p './database-seq/YNL122C.fasta'")
 
-# Statistics: Table I, Figure 8 and Figure 9
+# Database mode example
 # os.system("python3 main.py -v -e -k -f -s database-mode -n 200 './database-cerevisiae/'")
-# os.system("python3 main.py -v -e -k -f -s statistics-mode './database-cerevisiae/'")
 
-# Figure 10
-os.system("python3 main.py -a file-mode -p -sw -wl 351 -st 5 './database-seq/F56F11_4a.fasta'")
+# Figure 9
+os.system("python3 main.py -v -e -k -f -s file-mode -p -sw -wl 351 -st 5 './database-seq/F56F11_4a.fasta'")
